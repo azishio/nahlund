33c7fb4b768acb5622000e429e864f0832a1e098
azishio <azishio3141@gmail.com>
2024-10-15 13:10:48 +0900

    ✨ neo4jの実行時スクリプト

---
+++ neo4j_extension_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#!/bin/bash
+set -euC
+
+if [ -f /import/done ]; then
+    echo "Skip import process"
+    return
+fi
+
+# neo4jのdatabaseの削除
+echo "Start the database deletion process"
+rm -rf /data/databases
+rm -rf /data/transactions
+echo "Complete the database deletion process"
+
+# CSVデータのインポート
+echo "Start the data import process"
+bin/neo4j-admin database import \
+  --nodes=/import/river_node.csv \
+  --relationships=/import/river_link.csv \
+  --relationships=/import/delaunay.csv \
+  neo4j
+echo "Complete the data import process"
+
+# import処理の完了フラグファイルの作成
+echo "Start creating flag file"
+touch /import/done
+echo "Complete creating flag file"
+
+echo "Start ownership change"
+chown -R neo4j:neo4j /data
+chown -R neo4j:neo4j /logs
+echo "Complete ownership change"
